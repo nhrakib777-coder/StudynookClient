24b209ba8f888ce91bf2df6869fcb1d7ef764107
nhrakib777-coder <rakibnh93@gmail.com>
2026-05-21 13:37:53 +0800

    vercel setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // AUTOMATIC USES LOCAL OR VERCEL URL
+  baseURL: import.meta.env.VITE_API_URL 
+    || "https://studynookclient.vercel.app/api",
+
+  // Required for login/auth to work on Vercel
+  withCredentials: true,
 });
 
 export default api;
